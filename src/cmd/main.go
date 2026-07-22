193009989cb47e57048ea45e31471b884bcea4f0
Maksim Dachevsky <maxdachevsky4@gmail.com>
2026-07-22 17:54:07 +0300

    stage 1

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -3,13 +3,28 @@ package main
 import (
 	"fmt"
 	newssumbot "newssumbot/src/internal"
+	"time"
+
+	"gopkg.in/telebot.v4"
 )
 
 func main() {
-	resp, err := newssumbot.LoadToken()
+	token, err := newssumbot.LoadToken()
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	fmt.Println(resp)
+	pref := telebot.Settings{
+		Token:  token,
+		Poller: &telebot.LongPoller{Timeout: 10 * time.Second},
+	}
+	bot, _ := telebot.NewBot(pref)
+	bot.Handle("/ping", func(c telebot.Context) error {
+		return c.Send("pong")
+	})
+	bot.Handle("/start", func(c telebot.Context) error {
+		return c.Send("Bot is started and ready for use")
+	})
+	fmt.Println("INFO bot got started, pulling updates")
+	bot.Start()
 }
